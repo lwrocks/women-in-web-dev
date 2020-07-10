ead6af516a0054bb5ccda144751c2c43dad1a160
Laurel Wright <wright.laureld@gmail.com>
2020-07-10 22:16:52 +0200

    See you tomorrow

---
+++ jobsearch.js
@@ -0,0 +1,18 @@
+//  Setup Search & Filter Function
+function myFunction() {
+  var input, filter, ul, li, a, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUl");
+  li = document.getElementsByTagName("li");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = none;
+    }
+  }
+}
